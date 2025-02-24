c6b186bf50db5081b59392fa57a8b29819ea1eb3
Richard Howard <rich@risczero.com>
2025-02-23 16:47:40 -0800

    stop whining complier

--- ntt/parameters.cuh
+++ ntt/parameters.cuh
@@ -43,8 +43,8 @@ typedef size_t index_t;
 #define WINDOW_SIZE (1 << LG_WINDOW_SIZE)
 #define WINDOW_NUM ((MAX_LG_DOMAIN_SIZE + LG_WINDOW_SIZE - 1) / LG_WINDOW_SIZE)
 
-__device__ __constant__ fr_t forward_radix6_twiddles[32] = {};
-__device__ __constant__ fr_t inverse_radix6_twiddles[32] = {};
+__device__ __constant__ fr_t forward_radix6_twiddles[32];
+__device__ __constant__ fr_t inverse_radix6_twiddles[32];
 
 #if !defined(__CUDA_ARCH__) && !defined(__HIP_DEVICE_COMPILE__)
 # if defined(FEATURE_BLS12_377)
